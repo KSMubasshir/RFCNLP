ce1b425645f79042806b0c7fd7bc0e90244569ea
KSMubasshir <ksmubasshir@gmail.com>
2022-07-03 16:38:15 +0600

    pipeline updated to run FSM extraction

--- nlp2promela/nlp2promela.py
+++ nlp2promela/nlp2promela.py
@@ -9,12 +9,13 @@ usage      : python nlp2promela.py rfcs-annotated-tidied/TCP.xml
 import sys
 import os
 
-from stringUtils      import cleanFile
-from fsmUtils         import FSM
-from xmlUtils         import *
-from printUtils       import printTransition, printHeuristicRemoval
-from testConstants    import *
+from stringUtils import cleanFile
+from fsmUtils import FSM
+from xmlUtils import *
+from printUtils import printTransition, printHeuristicRemoval
+from testConstants import *
 from testPromelaModel import *
+
 
 # INPUT:
 #     we expect only a single argument, namely the path to the rfc .txt file
@@ -26,21 +27,21 @@ def main(writepromela=True, writepng=True):
     rfc = sys.argv[1]
     cleanedFile, removedLines = cleanFile(rfc)
     xml = ET.parse(cleanedFile)
-    
-    S  = list(set([a.text.upper() for a in xml.iter('def_state')]))
+
+    S = list(set([a.text.upper() for a in xml.iter('def_state')]))
     s0 = guessInitial(xml)
 
-    states  = [cleanUp(s) for s in S]
+    states = [cleanUp(s) for s in S]
     initial = cleanUp(s0)
 
-    promela_convenience = lambda x : "timeout" if x == "timeout" else x.upper()
+    promela_convenience = lambda x: "timeout" if x == "timeout" else x.upper()
 
-    id2state  = { a.get("id") : a.text for a in xml.iter('def_state') }
-    id2reason = { a.get("id") : promela_convenience(a.text) for a in xml.iter('def_event') }
+    id2state = {a.get("id"): a.text for a in xml.iter('def_state')}
+    id2reason = {a.get("id"): promela_convenience(a.text) for a in xml.iter('def_event')}
 
-    parsed = lambda t : parseTransition(t, xml, id2reason, id2state)
+    parsed = lambda t: parseTransition(t, xml, id2reason, id2state)
 
-    oktran = lambda A, b, c : len(A) > 0 and not None in [ b, c ]
+    oktran = lambda A, b, c: len(A) > 0 and not None in [b, c]
 
     transitions = []
 
@@ -54,7 +55,7 @@ def main(writepromela=True, writepng=True):
             if oktran(A, B, c):
                 for a in A:
                     source = a.replace("-", "_")
-                    dest   = c.replace("-", "_")
+                    dest = c.replace("-", "_")
                     if not (a == c and (B == 'ε' or B == ['ε'])):
                         transition = (source, B, dest)
                         transtuple = (transition, l)
@@ -70,14 +71,14 @@ def main(writepromela=True, writepng=True):
         ((source, B, dest), l) for ((source, B, dest), l) in transitions
         if ((len(B) == 1) and
             any(
-            [1 for ((s, BB, d), _) in transitions if 
-                (set(B).issubset(set(BB)) and
-                any([b for b in BB if b[-1] == "!"]) and
-                any([b for b in BB if b[-1] == "?"]) and
-                s == source                          and
-                d == dest)
-            ]
-        ))
+                [1 for ((s, BB, d), _) in transitions if
+                 (set(B).issubset(set(BB)) and
+                  any([b for b in BB if b[-1] == "!"]) and
+                  any([b for b in BB if b[-1] == "?"]) and
+                  s == source and
+                  d == dest)
+                 ]
+            ))
     ]
 
     # Heuristic: If we have x --(u)--> z and x --(ε)--> z, 
@@ -86,19 +87,18 @@ def main(writepromela=True, writepng=True):
     redundant_epsilon_artifacts = [
         ((source, B, dest), l) for ((source, B, dest), l) in transitions
         if (B == ['ε']) and
-            any(
-                [1 for ((s, BB, d), _) in transitions if 
+           any(
+               [1 for ((s, BB, d), _) in transitions if
                 (BB != ['ε'] and s == source and d == dest)])
     ]
 
     name = os.path.splitext(os.path.basename(rfc))[0]
 
     test_transitions = DCCP_transitions() if "DCCP" in name else \
-                       TCP_communication_transitions() + \
-                       TCP_user_call_transitions()     + \
-                       TCP_rst_transitions() if "TCP" in name else []
+        TCP_communication_transitions() + \
+        TCP_user_call_transitions() + \
+        TCP_rst_transitions() if "TCP" in name else []
 
-    
     for c in call_and_response_component_artifacts:
         printHeuristicRemoval(c, "call-and-response", test_transitions)
         transitions.remove(c)
@@ -107,25 +107,20 @@ def main(writepromela=True, writepng=True):
         printHeuristicRemoval(c, "redundant-epsilon", test_transitions)
         transitions.remove(c)
 
-    msgs = TCP_msgs()  if "TCP"  in name else \
-           DCCP_msgs() if "DCCP" in name else \
-           None
+    msgs = TCP_msgs() if "TCP" in name else \
+        DCCP_msgs() if "DCCP" in name else \
+            None
 
-    result = FSM(states,                    \
-                 initial,                   \
-                 transitions,               \
-                 labeled=True,              \
-                 removedLines=removedLines, \
-                 msgs=msgs)
+    result = FSM(states, initial, transitions, labeled=True, removedLines=removedLines, msgs=msgs)
 
     if "TCP" in name:
         result.save(writepng, writepromela, name, 2, TCPtranFilterHonest)
         testTCP(result, TCPtranFilterHonest)
         auto_evaluate_TCP_pml(name + ".pml")
         if writepromela:
-            result.save(False, True, name,              2, TCPtranFilterHonest)
-            TCP().save( False, True, name + "_CORRECT", 2, TCPtranFilterHonest)
-    
+            result.save(False, True, name, 2, TCPtranFilterHonest)
+            TCP().save(False, True, name + "_CORRECT", 2, TCPtranFilterHonest)
+
     elif "DCCP" in name:
         result.save(writepng, writepromela, name, 2)
         testDCCP(result)
@@ -133,9 +128,10 @@ def main(writepromela=True, writepng=True):
         if writepromela:
             result.save(False, True, name, 2)
             DCCP().save(False, True, name + "_CORRECT", 2)
-    
+
     else:
         result.save(writepng, writepromela, name)
+
 
 if __name__ == "__main__":
     main()
--- nlp2promela/testPromelaModel.py
+++ nlp2promela/testPromelaModel.py
@@ -1,40 +1,41 @@
 # coding: utf-8
-'''
+"""
 name       : testPromelaModel.py
 author     : [redacted]
 authored   : 17 October 2020
 description: tests the output TCP model
-'''
+"""
 
-from korg.Characterize import *
-from korg.Korg         import body
+from korg import *
 
-from printUtils        import makeGreen, makeRed
-from testConstants     import TCP
-from TCPtestConstants  import canonical_TCP_model, \
-                              canonical_TCP_IO,    \
-                              TCP_props_path,      \
-                              TCP_props,           \
-                              TCP_AP_prefix
+from printUtils import makeGreen, makeRed
+from testConstants import TCP
+from TCPtestConstants import canonical_TCP_model, \
+    canonical_TCP_IO, \
+    TCP_props_path, \
+    TCP_props, \
+    TCP_AP_prefix
 from DCCPtestConstants import canonical_DCCP_model, \
-                              canonical_DCCP_IO,    \
-                              DCCP_props_path,      \
-                              DCCP_props,           \
-                              DCCP_AP_prefix
+    canonical_DCCP_IO, \
+    DCCP_props_path, \
+    DCCP_props, \
+    DCCP_AP_prefix
 
 import os
 import glob
 import shutil
 
+
 def bigCleanUp(cur_model_name):
     for file in glob.glob("*.trail") + \
-                glob.glob("*tmp*")   + \
-                glob.glob("pan")     + \
-                glob.glob("*.pml")   + \
+                glob.glob("*tmp*") + \
+                glob.glob("pan") + \
+                glob.glob("*.pml") + \
                 glob.glob("._n_i_p_s_"):
         if file != cur_model_name + ".pml" and \
-           file != cur_model_name + "_CORRECT.pml":
+                file != cur_model_name + "_CORRECT.pml":
             os.remove(file)
+
 
 # Function to check if P |= phi
 def models2(P, phi, cur_model_name):
@@ -53,15 +54,18 @@ def models2(P, phi, cur_model_name):
                 fw.write(line)
     return check(tmpname)
 
+
 # Function to evaluate what properties TCP model |='s.
 def what_properties_does_TCP_model_support(P):
     return [models2(P, phi, "TCP") for phi in TCP_props]
+
 
 def choose_a_tmpname(P, midfix):
     tmpname = P.replace(".pml", "") + "." + midfix + ".pml"
     while os.path.isfile(tmpname):
         tmpname = str(abs(hash(tmpname))) + ".pml"
     return tmpname
+
 
 def add_AP_to(P, AP, tmpname, atomic_propositions_prefix):
     i = 0
@@ -88,59 +92,62 @@ def add_AP_to(P, AP, tmpname, atomic_propositions_prefix):
                     fw.write(line)
     return tmpname
 
+
 # Function to add atomic propositions to DCCP model.
 def add_atomic_propositions_to_DCCP_model(P):
     tmpname = choose_a_tmpname(P, "AP")
     AP = {
-        'CLOSED'   : ('ClosedState'  , 0), 
-        'LISTEN'   : ('ListenState'  , 0), 
-        'REQUEST'  : ('RequestState' , 0), 
-        'RESPOND'  : ('RespondState' , 0), 
-        'PARTOPEN' : ('PartOpenState', 0), 
-        'OPEN'     : ('OpenState'    , 0),
-        'CLOSEREQ' : ('CloseReqState', 0),
-        'CLOSING'  : ('ClosingState' , 0),
-        'TIMEWAIT' : ('TimeWaitState', 0), 
-        'STABLE'   : ('StableState'  , 0), 
-        'CHANGING' : ('ChangingState', 0), 
-        'UNSTABLE' : ('UnstableState', 0)
+        'CLOSED': ('ClosedState', 0),
+        'LISTEN': ('ListenState', 0),
+        'REQUEST': ('RequestState', 0),
+        'RESPOND': ('RespondState', 0),
+        'PARTOPEN': ('PartOpenState', 0),
+        'OPEN': ('OpenState', 0),
+        'CLOSEREQ': ('CloseReqState', 0),
+        'CLOSING': ('ClosingState', 0),
+        'TIMEWAIT': ('TimeWaitState', 0),
+        'STABLE': ('StableState', 0),
+        'CHANGING': ('ChangingState', 0),
+        'UNSTABLE': ('UnstableState', 0)
     }
     return add_AP_to(P, AP, tmpname, DCCP_AP_prefix)
+
 
 # Function to add atomic propositions to TCP model.
 def add_atomic_propositions_to_TCP_model(P):
     tmpname = choose_a_tmpname(P, "AP")
     AP = {
-        'CLOSED'       : ('ClosedState'   , 0),
-        'LISTEN'       : ('ListenState'   , 0),
-        'SYN_SENT'     : ('SynSentState'  , 0),
-        'SYN_RECEIVED' : ('SynRecState'   , 0),
-        'ESTABLISHED'  : ('EstState'      , 0),
-        'FIN_WAIT_1'   : ('FinW1State'    , 0),
-        'FIN_WAIT_2'   : ('FinW2State'    , 0),
-        'CLOSE_WAIT'   : ('CloseWaitState', 0),
-        'CLOSING'      : ('ClosingState'  , 0),
-        'LAST_ACK'     : ('LastAckState'  , 0),
-        'TIME_WAIT'    : ('TimeWaitState' , 0),
-        'end'          : ('EndState'      , 0)
+        'CLOSED': ('ClosedState', 0),
+        'LISTEN': ('ListenState', 0),
+        'SYN_SENT': ('SynSentState', 0),
+        'SYN_RECEIVED': ('SynRecState', 0),
+        'ESTABLISHED': ('EstState', 0),
+        'FIN_WAIT_1': ('FinW1State', 0),
+        'FIN_WAIT_2': ('FinW2State', 0),
+        'CLOSE_WAIT': ('CloseWaitState', 0),
+        'CLOSING': ('ClosingState', 0),
+        'LAST_ACK': ('LastAckState', 0),
+        'TIME_WAIT': ('TimeWaitState', 0),
+        'end': ('EndState', 0)
     }
     return add_AP_to(P, AP, tmpname, TCP_AP_prefix)
 
+
 def parse_2_peer_model(the_model):
-    network_code   = ""
+    network_code = ""
     remainder_code = ""
     with open(the_model, "r") as fr:
         in_network = False
         for line in fr:
-            if (line.strip() == "}" and in_network == True):
+            if line.strip() == "}" and in_network == True:
                 in_network = False
                 network_code += line
-            elif (line.strip() == "active proctype network() {"):
+            elif line.strip() == "active proctype network() {":
                 in_network = True
                 network_code += line
-            elif (in_network == False):
+            elif not in_network:
                 remainder_code += line
-            elif (in_network == True):
+            elif in_network:
                 network_code += line
     net_name = str(abs(hash(network_code))) + ".pml"
     rem_name = str(abs(hash(remainder_code))) + ".pml"
@@ -161,7 +168,7 @@ def parse_2_peer_model(the_model):
 
     print("WROTE TO " + dir_name + "/" + rem_name)
 
-    return (dir_name + "/" + net_name, dir_name + "/" + rem_name)
+    return dir_name + "/" + net_name, dir_name + "/" + rem_name
 
 
 def auto_evaluate_some_pml(some_pml, l, Phi, canonical_IO, canonical_model, cur_model_name):
@@ -172,59 +179,53 @@ def auto_evaluate_some_pml(some_pml, l, Phi, canonical_IO, canonical_model, cur_
     the_name = some_pml.replace(".pml", "")
     if "/" in the_name:
         the_name = the_name.split("/")[-1]
-    
+
     attack_these = set()
     for phi in Phi:
         support = models2(labeled, phi, cur_model_name)
-        if support == True:
+        if support:
             print(some_pml + makeGreen("⊨") + phi)
             attack_these.add(phi)
-        elif support == False:
+        elif not support:
             print(some_pml + makeRed("⊭") + phi)
         else:
             print("ERROR!!! support = " + str(support))
 
     for prop in sorted(list(attack_these)):
         bigCleanUp(cur_model_name)
-        
-        print("============ TRY TO ATTACK " + prop + "============")
-        print("remainder = " + remainder       )
-        print(     "prop = " + prop            )
-        print(  "network = " + network         )
-        print(       "IO = " + canonical_IO    )
-        
-        dirname = "attack-"                                       \
-                 + prop    .replace("/", ".").replace(".pml", "-") \
-                 + some_pml.replace("/", ".").replace(".pml", "-")
 
-        while (os.path.isdir("out/" + dirname)):
+        print("============ TRY TO ATTACK " + prop + "============")
+        print("remainder = " + remainder)
+        print("prop = " + prop)
+        print("network = " + network)
+        print("IO = " + canonical_IO)
+
+        dirname = "attack-" \
+                  + prop.replace("/", ".").replace(".pml", "-") \
+                  + some_pml.replace("/", ".").replace(".pml", "-")
+
+        while os.path.isdir("out/" + dirname):
             dirname = str(abs(hash(dirname))) + "." + dirname
 
-        body(remainder, 
-             prop, 
-             network, 
-             canonical_IO, 
+        body(remainder,
+             prop,
+             network,
+             canonical_IO,
              max_attacks=100,
-             with_recovery=True, 
-             name=dirname, 
+             with_recovery=True,
+             name=dirname,
              characterize=False,
              comparisons=[canonical_model])
-    
+
     bigCleanUp(cur_model_name)
 
+
 def auto_evaluate_DCCP_pml(DCCP_pml):
-    auto_evaluate_some_pml(DCCP_pml,                              \
-                           add_atomic_propositions_to_DCCP_model, \
-                           DCCP_props,                            \
-                           canonical_DCCP_IO,                     \
-                           canonical_DCCP_model,                  \
-                           "DCCP")
+    auto_evaluate_some_pml(DCCP_pml, add_atomic_propositions_to_DCCP_model, DCCP_props, canonical_DCCP_IO,
+                           canonical_DCCP_model, "DCCP")
+
 
 # Function to do automatic evaluation
 def auto_evaluate_TCP_pml(TCP_pml):
-    auto_evaluate_some_pml(TCP_pml,                              \
-                           add_atomic_propositions_to_TCP_model, \
-                           TCP_props,                            \
-                           canonical_TCP_IO,                     \
-                           canonical_TCP_model,                  \
-                           "TCP")
+    auto_evaluate_some_pml(TCP_pml, add_atomic_propositions_to_TCP_model, TCP_props, canonical_TCP_IO,
+                           canonical_TCP_model, "TCP")
